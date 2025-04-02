413cd668fde12d57e468dcad100b809b27774ef5
aferretti <a.ferretti@fertec.it>
2025-04-02 17:59:52 +0200

    Update 1.core.sh bug installazione

--- scripts/1.core.sh
+++ scripts/1.core.sh
@@ -30,6 +30,9 @@ setUsers() {
 }
 
 installBasePackages() {
+    //pacman -S base-devel dosfstools grub efibootmgr mtools less nano networkmanager openssh os-prober net-tools sudo glibc git udiskie --noconfirm --needed
+    //checkError "pacman -S base-devel dosfstools grub efibootmgr mtools nano networkmanager openssh os-prober net-tools sudo glibc git udiskie --noconfirm --needed"
+    
     pacman -S base-devel dosfstools grub efibootmgr mtools less nano networkmanager openssh os-prober net-tools sudo glibc git udiskie --noconfirm --needed
     checkError "pacman -S base-devel dosfstools grub efibootmgr mtools nano networkmanager openssh os-prober net-tools sudo glibc git udiskie --noconfirm --needed"
 

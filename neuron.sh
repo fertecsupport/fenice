a7f7963e615a680036e140d47e391420c9d0d16e
Riyad Labiad <r.labiad@fertec.it>
2026-07-07 18:20:05 +0200

    Reapply gateway and hostname parameters; verified unrelated to install issue

--- neuron.sh
+++ neuron.sh
@@ -4,4 +4,4 @@ pacman -Sy --noconfirm --needed git glibc
 git clone https://github.com/fertecsupport/fenice.git $HOME/neuron
  
 cd $HOME/neuron
-exec ./archinstall.sh neuron docker neuron "$1"
+exec ./archinstall.sh neuron docker "${3:-neuron}" "$1" "$2"
